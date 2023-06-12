1267a7d84e7b69a923b73d43b07ee399b3dc023c
vera-botoaca <126331242+vera-botoaca@users.noreply.github.com>
2023-06-12 23:54:58 +0300

    Update delete_last_occurence_of_item_in_linked_list.c

--- delete_last_occurence_of_item_in_linked_list.c
+++ delete_last_occurence_of_item_in_linked_list.c
@@ -1,5 +1,7 @@
 //Delete the last occurrence of an item from a linked list. 
 //So if the item is 7 and the list is [1,3,7,4,3,7,2], the result is [1,3,7,4,3,2].#include<stdio.h>
+/*Delete the last occurrence of an item from a linked list. 
+//So if the item is 7 and the list is [1,3,7,4,3,7,2], the result is [1,3,7,4,3,2].*/
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
@@ -17,26 +19,27 @@ void print_list(node_t *head)
     ptr=head;
     while(ptr!=NULL)
     {
-        printf("%d\n", ptr->data);
+        printf("%d ", ptr->data);
         ptr=ptr->link;
     }
     printf("\n");
 }
 
-void add(node_t **head, int data)
+void add_last_node(node_t **head, int data)
 {
     node_t *newnode;
     newnode=(node_t *)malloc(sizeof(node_t));
     newnode->data=data;
     newnode->link=NULL;
 
+    //daca lista e goala, noul nod devine capul listei
     if(*head==NULL)
     {
         *head=newnode;
         return;
     }
 
-    //parcurg fiecare element al listei pana la ultimul, apoi imi adaug noul nod
+    //acum, parcurgem lista pana la ultimul element, dupa care adaugam noul nod
     node_t *last_node;
     last_node=*head;
     while(last_node->link!=NULL)
@@ -46,61 +49,65 @@ void add(node_t **head, int data)
     last_node->link=newnode;
 }
 
-node_t *delete_last_occurence(node_t *head, int num)
+void add_first_node(node_t **head, int data)
 {
-    node_t *curr = head;
-    node_t *prev = NULL;
-    node_t *last_occurrence = NULL;
-    node_t *prev_occurrence = NULL;
+    node_t *newnode;
+    newnode=(node_t *)malloc(sizeof(node_t));
+    newnode->data=data;
+    newnode->link=*head;
 
-    if (curr == NULL)
+    *head=newnode;
+}
+
+node_t *delete_last_occurence_of_item(node_t *head, int num)
+{
+    node_t *temp = head;
+    node_t *last_occurrence = NULL;
+    node_t *prev = NULL;
+    while (temp != NULL)
     {
-        printf("Linked list is already empty!");
-        exit(1);
-    }
-    while (curr != NULL)
-    {
-        if (curr->data == num)
+        if (temp->data == num)
         {
-            last_occurrence = curr;
-            prev_occurrence = prev;
+            last_occurrence = temp;
         }
-        prev = curr;
-        curr = curr->link;
+        temp = temp->link;
     }
+    // Dacă nu s-a găsit nicio apariție a elementului, se returnează lista nemodificată
     if (last_occurrence == NULL)
     {
-        printf("Last occurrence of %d not found in the linked list.", num);
         return head;
     }
-    if (prev_occurrence == NULL)
+    temp = head;
+    while (temp != NULL && temp != last_occurrence)
     {
-        head = head->link;
+        prev = temp;
+        temp = temp->link;
+    }
+    if (prev != NULL)
+    {
+        prev->link = last_occurrence->link;
     }
     else
     {
-        prev_occurrence->link = last_occurrence->link;
+        head = last_occurrence->link;
     }
-
     free(last_occurrence);
     return head;
 }
+
 
 int main(void)
 {
     node_t *head;
     head=NULL;
-    add(&head, 1);
-    add(&head, 2);
-    add(&head, 3);
-    add(&head, 4);
-    add(&head, 2);
-
-    printf("Lista initiala este:\n");
+    add_last_node(&head, 3);
+    add_last_node(&head, 2);
+    add_last_node(&head, 3);
+    add_last_node(&head, 4);
+    add_last_node(&head, 3);
     print_list(head);
 
-    printf("Lista cu ultima aparitie a numarului 2 stearsa este:\n");
-    delete_last_occurence(head, 2);
+    head=delete_last_occurence_of_item(head, 3);
     print_list(head);
     return 0;
 }
