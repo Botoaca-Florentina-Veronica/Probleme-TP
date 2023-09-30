daceaa8eed5b13f64c75e059438b08abf6006fb8
Botoaca Florentina Veronica <126331242+Botoaca-Florentina-Veronica@users.noreply.github.com>
2023-09-30 20:33:42 +0300

    Update delete_even_nodes_in_linked_list.c

--- delete_even_nodes_in_linked_list.c
+++ delete_even_nodes_in_linked_list.c
@@ -1,9 +1,7 @@
-//C program to delete even nodes
-
-//met1
+//delete the even nodes in a linked list
 #include<stdio.h>
-#include<stdlib.h>
 #include<string.h>
+#include<stdlib.h>
 
 typedef struct node
 {
@@ -11,17 +9,20 @@ typedef struct node
     struct node *link;
 }node_t;
 
-
 void print_list(node_t *head)
 {
     node_t *ptr;
     ptr=head;
+    if(head==NULL)
+    {
+        printf("Linked list is empty!");
+        exit(1);
+    }
     while(ptr!=NULL)
     {
         printf("%d ", ptr->data);
         ptr=ptr->link;
     }
-    printf("\n");
 }
 
 void add_last_node(node_t **head, int data)
@@ -31,188 +32,78 @@ void add_last_node(node_t **head, int data)
     newnode->data=data;
     newnode->link=NULL;
 
-    //daca lista e goala, atunci noul meu nod devine capul listei
     if(*head==NULL)
     {
+        //daca lista este goala atunci noul element va deveni chiar capul listei
         *head=newnode;
         return;
     }
 
-    //acum parcurg lista pana la ultimul element, dupa care imi adaug noul nod
     node_t *last_node;
     last_node=*head;
     while(last_node->link!=NULL)
     {
         last_node=last_node->link;
     }
+    //daca ies din bucla while, atunci inseamna ca am parcurs toata lista pana la finalul ei
+    //deci acum vreau ca ultimul element al listei sa pointeze spre adresa noului nod
+    //de aici si linia de cod:
     last_node->link=newnode;
 }
 
-
-node_t *delete_even_nodes(node_t *head)
+node_t *delete_nodes(node_t *head)
 {
-    node_t *temp, *temp2;
-    temp = head;
-    temp2 = NULL;
+    node_t *curr, *prev;
+    curr=head;
+    prev=NULL;
 
-    // Verific dacă primul nod este par și îl șterg
-    while (temp != NULL && temp->data % 2 == 0)
+    while(curr!=NULL)
     {
-        head = temp->link;
-        free(temp);
-        temp = head;
-    }
-
-    // Parcurg restul listei și șterg nodurile pare
-    while (temp != NULL)
-    {
-        while (temp != NULL && temp->data % 2 != 0)
+        if(curr->data%2==0)
         {
-            temp2 = temp;
-            temp = temp->link;
+            if(prev==NULL)
+            {
+                //daca chiar capul listei este par atunci il vom sterge
+                //intai vom schimba adresa la care pointer ul head  "pointeaza" pentru ca daca o facem inainte
+                //riscam sa pierdem informatia pe care acesta o contine
+                //si astfel sa pierdem legaturile cu celelalte elemente ale listei
+                head=curr->link;
+                free(curr);  //aici eliberam memoria accesata de pointerul curent (care e si par)
+                curr=head->link; //*1.
+            }
+            else
+            //daca ajungem aici inseamna ca avem si alte noduri pare inafara de capul listei
+            {
+                prev->link=curr->link;
+                free(curr);
+                curr=prev->link;  // *2.   aici ca si la *1 am scris aceasta linie de cod fiindca pointerul meu curr trebuie
+                // mereu schimbat la urmatorul, nu sa stea pe loc
+            }
         }
-
-        if (temp == NULL)
-            break;
-
-        temp2->link = temp->link;
-        free(temp);
-        temp = temp2->link;
+        else
+        //daca ajungem aici inseamna ca am trecut cu bine de posibilitatea de a avea numere pare in lista
+        //deci nu ne mai ramane decat sa o parcurgem mai departe
+        {
+            prev=curr; //nu vrem sa lasam pointerul anterior sa fie egal cu null, trebuie updatat, la fel si curr dupa cum urmeaza:
+            curr=curr->link;  //ce am incercat sa facem la *1 si *2
+        }
     }
-
     return head;
 }
 
-
-void add_first_node(node_t **head, int data)
-{
-    node_t *newnode;
-    newnode=(node_t *)malloc(sizeof(node_t));
-    newnode->data=data;
-    newnode->link=*head;
-
-    *head=newnode;
-}
-
-
 int main(void)
 {
-    node_t *head;
-    head=NULL;
+    node_t *head=NULL;
+    printf("Lista initiala:\n");
     add_last_node(&head, 1);
     add_last_node(&head, 2);
     add_last_node(&head, 3);
     add_last_node(&head, 4);
-    add_first_node(&head, 0);
+    add_last_node(&head, 5);
     print_list(head);
 
-    head=delete_even_nodes(head);
-    print_list(head);
-    return 0;
-}
-
-
-//met 2
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-
-typedef struct node
-{
-    int data;
-    struct node *link;
-}node_t;
-
-
-void add(node_t **head, int data)
-{
-    node_t *newnode;
-    newnode=(node_t *)malloc(sizeof(node_t));
-    newnode->data=data;
-    newnode->link=NULL;
-
-    if(*head==NULL)
-    {
-        *head=newnode;
-        return;
-    }
-
-    //parcurgem fiecare element pana la finalul listei
-    node_t *last_node;
-    last_node=*head;
-    while(last_node->link!=NULL)
-    {
-        last_node=last_node->link;
-    }
-    last_node->link=newnode;
-}
-
-// Function to delete even nodes from the linked list
-node_t *delete_even_node(node_t *head)
-{
-    if (head == NULL)
-    {
-        printf("Linked list is already empty!");
-        exit(1);
-    }
-
-    node_t *curr = head;
-    node_t *prev = NULL;
-
-    while (curr != NULL) 
-    {
-        if (curr->data % 2 == 0) 
-        {
-            if (prev == NULL) 
-            {
-                // Deleting the first node if it is even
-                head = curr->link;
-                free(curr);
-                curr = head;
-            } 
-            else 
-            {
-                // Deleting an even node, other than the first node
-                prev->link = curr->link;
-                free(curr);
-                curr = prev->link;
-            }
-        } 
-        else 
-        {
-            prev = curr;
-            curr = curr->link;
-        }
-    }
-    return head;
-}
-
-
-void print_list(node_t *head)
-{
-    node_t *ptr;
-    ptr=head;
-    while(ptr!=NULL)
-    {
-        printf("%d\n", ptr->data);
-        ptr=ptr->link;
-    }
-    printf("\n");
-}
-
-int main(void)
-{
-    node_t *head;
-    head=NULL;
-    add(&head, 1);
-    add(&head, 2);
-    add(&head, 3);
-    add(&head, 4);
-    printf("Lista initial este:\n");
-    print_list(head);
-
-    printf("Lista cu elementele pare sterse este:\n");
-    delete_even_node(head);
+    printf("Lista dupa stergeri:\n");
+    head=delete_nodes(head);
     print_list(head);
     return 0;
 }
